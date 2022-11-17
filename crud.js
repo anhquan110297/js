cadda30b90d343275de751c23d54c9c4f3edb92f
Quan <taqdnc@gmail.com>
2022-11-17 21:07:44 +0700

    update crud

--- crud.js
+++ crud.js
@@ -38,46 +38,45 @@ let students = [
 
 // pop, shift, push, unshift, splice, concat, slice;
 // Create
-// let studentName = prompt("please input student's name");
-// let studentAddress = prompt("please input student's address");
+let studentName = prompt("please input student's name");
+let studentAddress = prompt("please input student's address");
 
-// if (students.length == 0){
-//     students.push({
-//         id : 1,
-//         name : studentName,
-//         diachi : studentAddress
-//     });
-// } else {
-//     students.push({
-//         id : (students[students.length-1].id + 1),
-//         name : studentName,
-//         diachi : studentAddress
-//     })
-// }
-// console.log(students);
+if (students.length == 0){
+    students.push({
+        id : 1,
+        name : studentName,
+        diachi : studentAddress
+    });
+} else {
+    students.push({
+        id : (students[students.length-1].id + 1),
+        name : studentName,
+        diachi : studentAddress
+    })
+}
+console.log(students);
 
 
 // edit
 // gia su o day e chi cho edit name thoi
 let tempArray = [];
 students.forEach(student => tempArray.push(student.id));
-// console.log(tempArray);
-// let studentIdEdit = parseInt(prompt("please input student id for edit"));
-// console.log(studentIdEdit);
-// if (isNaN(studentIdEdit)){
-//     alert("please input a number test");
-// };
-// if (!tempArray.includes(studentIdEdit)){
-//     alert("don't have student have id " + studentIdEdit);
-// }
-// for (let i = 0; i < students.length ; i++){
-//     if (students[i].id == studentIdEdit ){
-//         students[i].name = prompt("please input student's name");
-//     }
-// };
-// console.log(students);
+console.log(tempArray);
+let studentIdEdit = parseInt(prompt("please input student id for edit"));
+console.log(studentIdEdit);
+if (isNaN(studentIdEdit)){
+    alert("please input a number test");
+};
+if (!tempArray.includes(studentIdEdit)){
+    alert("don't have student have id " + studentIdEdit);
+}
+for (let i = 0; i < students.length ; i++){
+    if (students[i].id == studentIdEdit ){
+        students[i].name = prompt("please input student's name");
+    }
+};
+console.log(students);
 
-// delete gia su se xoa sv co id 5
 let studentIdDelete = parseInt(prompt("please input student's id for delete"));
 console.log(students);
 if (!tempArray.includes(studentIdDelete)){
@@ -92,4 +91,19 @@ if (!tempArray.includes(studentIdDelete)){
 }
 
 console.log(students);
+
+// tim 1 sinh vien 
+
+let studentIdSearch = parseInt(prompt("please input student id for search"));
+
+if (!tempArray.includes(studentIdSearch)){
+    alert("don't have student have id " + studentIdSearch);
+} else {
+    students.forEach((student) => {
+        if (student.id == studentIdDelete){
+            console.log(student);
+            return;
+        }
+    });
+}
 
